309afb22b0bbacb984f2f2231080fca20a62e6d0
zhumabek21042000 <zhumabek.gubash@gmail.com>
2021-06-26 21:05:35 +0500

    added redirect

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AppMain /> */}
-    <App/>
+    <AppMain />
+    {/* <App/> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
