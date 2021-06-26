c00532487625e2b45aa6f40ed75d0ab0cac1694a
zhumabek21042000 <zhumabek.gubash@gmail.com>
2021-06-26 22:15:12 +0500

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from "react-router-dom";
 import AppMain from './components/app/app';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <AppMain />
+    </BrowserRouter>
     {/* <App/> */}
   </React.StrictMode>,
   document.getElementById('root')
